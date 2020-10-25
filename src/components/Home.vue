b0f807ac0dc98f6b0cd7aab9b2deb42df70d8658
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-10-25 10:02:43 -0300

    fix: organize file

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,5 @@
 <template>
+  {{ res }}
   <form>
     <AppTitle />
     <SearchBar />
--- src/components/SeachBar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <input type="text" id="searchBar" />
-  <a href="#">
-    <!-- <img src="" alt=""> -->
-    Pesquisar
-  </a>
-</template>
-
-<style></style>
