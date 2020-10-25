3191efaf99fd7828d11ef68f896387bcca987d13
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-10-25 11:25:36 -0300

    feat: create component Result

---
+++ src/components/Result.vue
@@ -0,0 +1,25 @@
+<template>
+  <form id="result">
+    <AppTitle />
+    <SearchBar />
+  </form>
+</template>
+
+<script>
+import AppTitle from "./AppTitle";
+import SearchBar from "./SearchBar";
+export default {
+  components: {
+    AppTitle,
+    SearchBar,
+  },
+};
+</script>
+
+<style>
+#result {
+  background: blueviolet;
+  display: flex;
+  flex-flow: row nowrap;
+}
+</style>
