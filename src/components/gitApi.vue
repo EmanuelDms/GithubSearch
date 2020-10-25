488a47523bdd33c1efaaf5ef9ea316a3b46a8b8d
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-10-25 13:40:37 -0300

    feat: create component do Github API

---
+++ src/components/gitApi.vue
@@ -0,0 +1,23 @@
+<template>
+  {{ res }}
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      res: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.github.com/users/emanueldms")
+      .then((response) => (this.res = response));
+  },
+};
+</script>
+
+<style>
+</style>
