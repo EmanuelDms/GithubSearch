fc13a3aa7094e2b50fe80c0e71bafe08a2175d1b
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-10-25 16:19:15 -0300

    feat: add element and style * .form-group including search contentss * .form-group row nowrap and some styles

--- src/views/SearchBar.vue
+++ src/views/SearchBar.vue
@@ -1,6 +1,33 @@
 <template>
-  <input type="text" id="searchBar" />
-  <a href="#">Pesquisar</a>
+  <div class="form-group">
+    <input type="text" id="searchBar" placeholder="Type a username..." />
+    <router-link to="/result" id="linkResult">
+      <img src="../assets/search.svg" alt="search image" />
+    </router-link>
+  </div>
 </template>
 
-<style></style>
+<style>
+.form-group {
+  display: flex;
+  flex-flow: row nowrap;
+}
+
+#searchBar {
+  width: 600px;
+  height: 50px;
+
+  padding: 15px;
+  border: 2px solid black;
+}
+
+#linkResult {
+  background-color: black;
+  width: 100px;
+  height: 50px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
